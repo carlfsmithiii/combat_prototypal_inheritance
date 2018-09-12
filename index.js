d484b346c9e6642a2738cb587a06f4c8e8e4b516
Carl Smith <carlfsmithiii@gmail.com>
2018-09-12 16:32:43 -0400

    adjusted opponent attack force

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ Character.prototype.befriend = function(target) {
 
 const you = new Character({name: 'You', health: 10, attackingForce: 3, healingRate: 3, friendliness: 5});
 
-const opponent = new Character({name: 'The Monster', health: 6, attackingForce: 8, healingRate: 2, friendliness: 1});
+const opponent = new Character({name: 'The Monster', health: 6, attackingForce: 15, healingRate: 2, friendliness: 1});
 
 document.getElementById("attack").addEventListener('click', function () {
     updateYourMove("You attack! " + you.attack(opponent));
@@ -47,7 +47,7 @@ document.getElementById("heal").addEventListener("click", function () {
 
 document.getElementById("make-friends").addEventListener('click', function () {
     updateYourMove("You try to make friends. " + you.befriend(opponent));
-    handleOpponenetsTurn(); 
+    handleOpponentsTurn(); 
 });
 
 function handleOpponentsTurn() {
